11f4f0e0cd15c510aa4908b4a6241895b61a756a
Tommy Fisher <tommybfisher@gmail.com>
2016-04-24 14:01:56 +0300

    Keyboard shows for ios

--- app/utils/directives/auto-focus.directive.ts
+++ app/utils/directives/auto-focus.directive.ts
@@ -1,12 +1,13 @@
-import {Directive, ElementRef} from 'angular2/core';
+import {Directive, ElementRef, ViewChildren} from 'angular2/core';
 
 @Directive({
     selector: '[bglAutoFocus]'
 })
 export class AutoFocusDirective {
     constructor(private elem: ElementRef) {
-        window.setTimeout(function() {
-            elem.nativeElement.querySelector('input').focus();
-        });
+        this.elem = elem;
+    }
+    ngAfterViewInit() {
+        this.elem.nativeElement.querySelector('input').focus();
     }
 }
